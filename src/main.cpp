912c8a24f5e454fcd8eb052d31f65275601b60b0
Ivan Kuznetsov <ikuznetsov511@gmail.com>
2025-10-27 21:58:40 +0200

    pre release-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,8 @@
-//
-// Created by iznets on 9/20/25.
-//
-#include <iostream>  // for std::cin and std::cout
-#include <string>    // for std::string
-#include "../include/entities/Customer.h"
+#include "StoreService.h"
 
 int main() {
-    std::string userInput;
-
-    std::cout << "Enter username: ";
-    std::cin >> userInput;
-
-    Customer customer(userInput);
-
-    int balance;
-    std::cout << "Enter balance: ";
-    std::cin >> balance;
-
-    customer.add_money(balance);
-    customer.get_balance();
+    StoreService store;
+    store.loginMenu();
 
     return 0;
 }
